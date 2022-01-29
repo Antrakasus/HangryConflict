7d2f79aa3fbfdd808a5e2c66a48cbb0fc8fe8953
Antrakasus <antrakasus@gmail.com>
2022-01-29 19:47:45 +0100

    beep boop

--- main.js
+++ main.js
@@ -11,9 +11,17 @@ console.log("Listening")
 function angy(req, res){
     if(req.method=="POST"){
         res = req.ip;
-    }
-    if(req.method=="GET"){
-        res.render("index");
+    }else if(req.method=="GET"){
+        if(req.path=="/"){
+            res.render("index")
+        }
+        else if(req.body.want=="ip"){
+            res.set("Content-Type", "text/plain")
+            res.send(req.ip)
+        }
+        else{
+            res.sendFile(req.path)
+        }
     }
     console.log(req.path)
 }
