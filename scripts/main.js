f978c79cdb3ec2855dc2130b4cb54f7b8eebf17e
= <=>
2022-03-03 16:50:36 +0100

    beep boop

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,11 @@ var req = new XMLHttpRequest();
 function response(){
     console.log(req.status);
     console.log(req.responseText);
-    if(req.status==200){
-        ipp.innerHTML = "Your ip is: "+req.responseText.split(":")[3];
-    }
+    ipp.innerHTML = "Your ip is: "+req.responseText.split(":")[3];
 
 }
-req.onload=response();
+req.addEventListener("load", response);
+req.addEventListener("error", alert("Request Failed, please try again"));
 function send(){
     req.open("POST", window.location);
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
